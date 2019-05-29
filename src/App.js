a8d2d783e2cdcb4b88720b3ff47fbb171f89ac30
심응준 <sim-eungjun@sim-eungjun-ui-Mac-Pro.local>
2019-05-29 11:10:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Slide_1 from './slides/Slide_1';
 import Slide_2 from './slides/Slide_2';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 function App() {
 
@@ -10,14 +11,8 @@ function App() {
 
   return (
     <div className="App">      
-      <Router basename={process.env.PUBLIC_URL}>  
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/1">Slide1</Link></li>
-          <li><Link to="/2">Slide2</Link></li>
-        </ul>
-      </nav>        
+      <Router>
+        <Route exact path = "/" component={Home}/>
         <Route path = "/1" component={Slide_1}/>
         <Route path = "/2" component={Slide_2}/>
       </Router>
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class Home extends Component{
+
+    render(){
+        const style={
+            width:"100%",
+            height:"100%",
+            backgroundColor:"darkblue",
+        }
+
+        return(
+            <div style={style}> Home </div>
+        )
+    }
+}
+
+
+export default Home
